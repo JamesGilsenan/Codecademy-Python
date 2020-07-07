f032ca023ffa5a4277aedd5b8e5596e395c2d76e
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-07 20:15:55 +0100

    Added function max_num

--- Loops/Loops_Challenges.py
+++ Loops/Loops_Challenges.py
@@ -51,6 +51,13 @@ def over_nine_thousand(lst):
           break
     return total
 
+def max_num(nums):
+    max = 0
+    for num in nums:
+        if num > max:
+            max = num
+    return max
+
 print(divisible_by_ten([20, 25, 30, 35, 40]))
 print(add_greetings(["Tom", "Dick", "Harry"]))
 print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
@@ -58,3 +65,4 @@ print(odd_indices([4, 3, 7, 10, 11, -2]))
 print(exponents([2, 3, 4], [1, 2, 3]))
 print(larger_sum([1, 9, 5], [2, 3, 7]))
 print(over_nine_thousand([8000, 900, 120, 5000]))
+print(max_num([50, -10, 0, 75, 20]))
