b54e1d6a74c88231d162cf86bfd353dcb292bf33
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-07 16:41:13 +0100

    Added function odd_indicies

--- Loops/Loops_Challenges.py
+++ Loops/Loops_Challenges.py
@@ -17,7 +17,15 @@ def delete_starting_evens(lst):
         lst = lst[1:]
     return lst
 
+def odd_indices(lst):
+    odd_index_lst = []
+    for i in range(len(lst)):
+        if i % 2 == 1:
+            odd_index_lst.append(lst[i])
+    return odd_index_lst
+
 
 print(divisible_by_ten([20, 25, 30, 35, 40]))
 print(add_greetings(["Tom", "Dick", "Harry"]))
 print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(odd_indices([4, 3, 7, 10, 11, -2]))
