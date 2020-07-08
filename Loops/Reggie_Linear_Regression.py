30c40562cb1219d11b02dbaf867a2dbb7968b8da
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-08 13:53:52 +0100

    Added calculate_all_error function.

--- Loops/Reggie_Linear_Regression.py
+++ Loops/Reggie_Linear_Regression.py
@@ -9,6 +9,13 @@ def calculate_error(m, b, point):
     difference = y_value - y_point
     return abs(difference)
 
+def calculate_all_error(m, b, points):
+    total_error = 0
+    for point in points:
+        total_error += calculate_error(m, b, point)
+    return total_error
+
+
 print(get_y(1, 0, 7) == 7)
 print(get_y(5, 10, 3) == 25)
 #this is a line that looks like y = x, so (3, 3) should lie on it. thus, error should be 0:
@@ -19,3 +26,21 @@ print(calculate_error(1, 0, (3, 4)))
 print(calculate_error(1, -1, (3, 3)))
 #the point (3, 3) should be 5 units away from the line y = -x + 1:
 print(calculate_error(-1, 1, (3, 3)))
+
+#every point in this dataset lies upon y=x, so the total error should be zero:
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(1, 0, datapoints))
+
+#every point in this dataset is 1 unit away from y = x + 1, so the total error should be 4:
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(1, 1, datapoints))
+
+#every point in this dataset is 1 unit away from y = x - 1, so the total error should be 4:
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(1, -1, datapoints))
+
+
+#the points in this dataset are 1, 5, 9, and 3 units away from y = -x + 1, respectively, so total error should be
+# 1 + 5 + 9 + 3 = 18
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(-1, 1, datapoints))
