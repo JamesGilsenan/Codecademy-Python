ad40ad10b43ecf10b20b12e154dcb137a01ed2fa
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-14 22:15:46 +0100

    Added function unique_english_letters

---
+++ Strings/Strings_Challenges.py
@@ -0,0 +1,12 @@
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+def unique_english_letters(word):
+  uniques = 0
+  for letter in letters:
+    if letter in word:
+      uniques += 1
+  return uniques
+
+print(unique_english_letters("mississippi"))
+# should print 4
+print(unique_english_letters("Apple"))
+# should print 4
