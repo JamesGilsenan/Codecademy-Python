81eeb5bbd77e8a8769f521f22592b5774c52aa86
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-15 13:32:55 +0100

    Added function count_char_x

--- Strings/Strings_Challenges.py
+++ Strings/Strings_Challenges.py
@@ -6,7 +6,19 @@ def unique_english_letters(word):
       uniques += 1
   return uniques
 
+def count_char_x(word, x):
+  count_x = 0
+  for i in range(len(word)):
+    if word[i] == x:
+      count_x += 1
+  return count_x
+
 print(unique_english_letters("mississippi"))
 # should print 4
 print(unique_english_letters("Apple"))
 # should print 4
+
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
+# should print 1
