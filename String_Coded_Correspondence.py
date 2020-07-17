5f4f2a412a10e73a018c9882589e4a575fe343ce
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-17 16:24:35 +0100

    Added function decrypt_vigenère

--- String_Coded_Correspondence.py
+++ String_Coded_Correspondence.py
@@ -38,12 +38,29 @@ def brute_force_caeser(message):
         print("\nOffset = " + str(offset) + " - " + decrypted_message) 
 
 def decrypt_vigenère(message, keyword):
-    #method body
+    punctuation = "!?.,' "
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    decrypted_message = ""
+    keyphrase = ""
+    index = 0
+    for i in range(len(message)):
+        if not message[i] in punctuation:
+            keyphrase += keyword[index % len(keyword)]
+            index += 1
+
+            letter_value = alphabet.find(message[i])
+            key_value = alphabet.find(keyphrase[i])
+            decrypted_message += alphabet[letter_value - key_value]
+        else:
+            keyphrase += message[i]
+            decrypted_message += message[i]
+    print(keyphrase)
+    return decrypted_message
 
 
 #print(decrypt_caeser_message("xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!", 10))
 #print(encrypt_caeser_message("hey there! I have decrypted your message. This Caeser Cipher is really cool!", 10))
 #print(decrypt_caeser_message("jxu evviuj veh jxu iusedt cuiiqwu yi vekhjuud.", 10))
 #print(decrypt_caeser_message("bqdradyuzs ygxfubxq omqemd oubtqde fa oapq kagd yqeemsqe ue qhqz yadq eqogdq!", 14))
-brute_force_caeser("vhfinmxkl atox kxgwxkxw tee hy maxlx hew vbiaxkl tl hulhexmx. px'ee atox mh kxteer lmxi ni hnk ztfx by px ptgm mh dxxi hnk fxlltzxl ltyx.")
-decrypt_vigenère("dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!", "friends")
+#brute_force_caeser("vhfinmxkl atox kxgwxkxw tee hy maxlx hew vbiaxkl tl hulhexmx. px'ee atox mh kxteer lmxi ni hnk ztfx by px ptgm mh dxxi hnk fxlltzxl ltyx.")
+print(decrypt_vigenère("dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!", "friends"))
