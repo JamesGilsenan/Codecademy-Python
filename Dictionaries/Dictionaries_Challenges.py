0613ba8734e453ab439934ce3f8a3707a726374c
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-23 21:54:39 +0100

    Added function frequency_dictionary

--- Dictionaries/Dictionaries_Challenges.py
+++ Dictionaries/Dictionaries_Challenges.py
@@ -40,33 +40,46 @@ def word_length_dictionary(words):
     words_dictionary = {key:value for key, value in zip(words, word_lengths)}
     return words_dictionary
 
+def frequency_dictionary(words):
+    word_frequency = {}
+    count = 0
+    for word in words:
+        count = word_frequency.get(word, 0)
+        word_frequency[word] = count + 1
+    return word_frequency
 
-print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+
+#print(sum_values({"milk":5, "eggs":2, "flour": 3}))
 # should print 10
-print(sum_values({10:1, 100:2, 1000:3}))
+#print(sum_values({10:1, 100:2, 1000:3}))
 # should print 6
 
-print(sum_even_keys({1:5, 2:2, 3:3}))
+#print(sum_even_keys({1:5, 2:2, 3:3}))
 # should print 2
-print(sum_even_keys({10:1, 100:2, 1000:3}))
+#print(sum_even_keys({10:1, 100:2, 1000:3}))
 # should print 6
 
-print(add_ten({1:5, 2:2, 3:3}))
+#print(add_ten({1:5, 2:2, 3:3}))
 # should print {1:15, 2:12, 3:13}
-print(add_ten({10:1, 100:2, 1000:3}))
+#print(add_ten({10:1, 100:2, 1000:3}))
 # should print {10:11, 100:12, 1000:13}
 
-print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+#print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 # should print [1, 4]
-print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+#print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
 # should print ["a"]
 
-print(max_key({1:100, 2:1, 3:4, 4:10}))
+#print(max_key({1:100, 2:1, 3:4, 4:10}))
 # should print 1
-print(max_key({"a":100, "b":10, "c":1000}))
+#print(max_key({"a":100, "b":10, "c":1000}))
 # should print "c"
 
-print(word_length_dictionary(["apple", "dog", "cat"]))
+#print(word_length_dictionary(["apple", "dog", "cat"]))
 # should print {"apple":5, "dog": 3, "cat":3}
-print(word_length_dictionary(["a", ""]))
+#print(word_length_dictionary(["a", ""]))
 # should print {"a": 1, "": 0}
+
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+# should print {"apple":2, "cat":1, 1:1}
+print(frequency_dictionary([0,0,0,0,0]))
+# should print {0:5}
