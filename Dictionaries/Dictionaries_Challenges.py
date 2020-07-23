8fdd351ae3076fcd8f6d74f256a7ce5bc1b69631
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-23 21:14:18 +0100

    Added function values_that_are_keys

--- Dictionaries/Dictionaries_Challenges.py
+++ Dictionaries/Dictionaries_Challenges.py
@@ -16,6 +16,14 @@ def add_ten(my_dictionary):
         my_dictionary[key] += 10
     return my_dictionary
 
+def values_that_are_keys(my_dictionary):
+  matches = []
+  for key in my_dictionary:
+    for value in my_dictionary.values():
+      if value == key:
+        matches.append(value)
+  return matches
+
 print(sum_values({"milk":5, "eggs":2, "flour": 3}))
 # should print 10
 print(sum_values({10:1, 100:2, 1000:3}))
@@ -30,3 +38,8 @@ print(add_ten({1:5, 2:2, 3:3}))
 # should print {1:15, 2:12, 3:13}
 print(add_ten({10:1, 100:2, 1000:3}))
 # should print {10:11, 100:12, 1000:13}
+
+print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]
