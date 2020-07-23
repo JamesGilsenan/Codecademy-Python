c0c09dd012c9ede03798f1748dd21674869c1bb0
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-23 18:15:37 +0100

    Added dictionary player to points. Populated it

--- Dictionaries/Dictionaries_Scrabble.py
+++ Dictionaries/Dictionaries_Scrabble.py
@@ -18,4 +18,12 @@ print(brownie_points)
 
 player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"],
  "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
- 
+player_to_points = {}
+
+for player, words in player_to_words.items():
+    player_points = 0
+    for word in words:
+        player_points += score_word(word, letter_to_points)
+    player_to_points[player] = player_points
+
+print(player_to_points)
