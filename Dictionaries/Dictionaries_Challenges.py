6beedc8f349f9a90468b2a6eda95e8084da755bd
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-23 19:39:35 +0100

    Added function sum_even_keys

--- Dictionaries/Dictionaries_Challenges.py
+++ Dictionaries/Dictionaries_Challenges.py
@@ -3,8 +3,20 @@ def sum_values(my_dictionary):
   for element in my_dictionary.values():
     total += element
   return total
-  
+
+def sum_even_keys(my_dictionary):
+    total = 0
+    for element in my_dictionary:
+        if element % 2 == 0:
+            total += my_dictionary[element]
+    return total
+
 print(sum_values({"milk":5, "eggs":2, "flour": 3}))
 # should print 10
 print(sum_values({10:1, 100:2, 1000:3}))
+# should print 6
+
+print(sum_even_keys({1:5, 2:2, 3:3}))
+# should print 2
+print(sum_even_keys({10:1, 100:2, 1000:3}))
 # should print 6
