b0e7953ec09f2eb4dc0d33266a27f4dfd8ec8f29
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-23 18:42:30 +0100

    Refactored code. Players points is now a function

--- Dictionaries/Dictionaries_Scrabble.py
+++ Dictionaries/Dictionaries_Scrabble.py
@@ -11,26 +11,32 @@ def score_word(word, dictionary):
     return score
 
 def play_word(player, word):
-    word.upper()
+    word = word.upper()
     player_to_words[player].append(word)
+
+def update_point_totals(player_to_words):
+    player_to_points = {}
+    for player, words in player_to_words.items():
+        player_points = 0
+        for word in words:
+            player_points += score_word(word, letter_to_points)
+        player_to_points[player] = player_points
+    return player_to_points
 
 
 letter_to_points = {key:value for key, value in zip(letters, points)}
 letter_to_points[" "] = 0
 print(letter_to_points)
 brownie_points = score_word("brownie", letter_to_points)
-print(brownie_points)
+#print(brownie_points)
 
 player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"],
  "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
-player_to_points = {}
 
-for player, words in player_to_words.items():
-    player_points = 0
-    for word in words:
-        player_points += score_word(word, letter_to_points)
-    player_to_points[player] = player_points
-
-print(player_to_points)
-play_word("wordNerd", "brownie")
 print(player_to_words)
+print(update_point_totals(player_to_words))
+play_word("wordNerd", "brownie")
+play_word("player1", "winning")
+play_word("Prof Reader", "fox")
+print(player_to_words)
+print(update_point_totals(player_to_words))
