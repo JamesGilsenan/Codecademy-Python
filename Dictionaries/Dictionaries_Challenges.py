0731cb19de3b49ee7e1d570f8cbc0b48ba26684c
James Gilsenan <jamesgilsenan37@gmail.com>
2020-07-23 19:44:18 +0100

    Added function add_ten

--- Dictionaries/Dictionaries_Challenges.py
+++ Dictionaries/Dictionaries_Challenges.py
@@ -11,6 +11,11 @@ def sum_even_keys(my_dictionary):
             total += my_dictionary[element]
     return total
 
+def add_ten(my_dictionary):
+    for key in my_dictionary:
+        my_dictionary[key] += 10
+    return my_dictionary
+
 print(sum_values({"milk":5, "eggs":2, "flour": 3}))
 # should print 10
 print(sum_values({10:1, 100:2, 1000:3}))
@@ -20,3 +25,8 @@ print(sum_even_keys({1:5, 2:2, 3:3}))
 # should print 2
 print(sum_even_keys({10:1, 100:2, 1000:3}))
 # should print 6
+
+print(add_ten({1:5, 2:2, 3:3}))
+# should print {1:15, 2:12, 3:13}
+print(add_ten({10:1, 100:2, 1000:3}))
+# should print {10:11, 100:12, 1000:13}
