5169ad7706849a589e724db3478cab2b2c767d66
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-07-27 16:02:56 +0100

    Added function calculate_availability

--- Dictionaries/Abruptly_Goblins_Planner.py
+++ Dictionaries/Abruptly_Goblins_Planner.py
@@ -1,15 +1,25 @@
 gamers = []
 
 def add_gamer(gamer, gamers_list):
-    if "name" in gamer and "availability" in gamer:
+    if gamer.get("name") and gamer.get("availability"):
         gamers_list.append(gamer)
-    return gamers_list
+    else:
+        print("Gamer missing critical information")
 
 def build_daily_frequency_table():
-    return {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0,}
+    frequency_table = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0,}
+    return frequency_table
 
-kimberly = {"name": "Kimberly Warner", "availability": ["Monday", "Tuesday", "Firday"]}
-#print(add_gamer(kimberly, gamers))
+def calculate_availability(gamers_list, availability_frequency):
+    for gamer in gamers_list:
+        for day in gamer["availability"]:
+            availability_frequency[day] += 1
+    return availability_frequency
+
+
+
+kimberly = {"name": "Kimberly Warner", "availability": ["Monday", "Tuesday", "Friday"]}
+(add_gamer(kimberly, gamers))
 add_gamer({'name':'Thomas Nelson','availability': ["Tuesday", "Thursday", "Saturday"]}, gamers)
 add_gamer({'name':'Joyce Sellers','availability': ["Monday", "Wednesday", "Friday", "Saturday"]}, gamers)
 add_gamer({'name':'Michelle Reyes','availability': ["Wednesday", "Thursday", "Sunday"]}, gamers)
@@ -18,7 +28,9 @@ add_gamer({'name': 'Joanne Lynn', 'availability': ["Monday", "Thursday"]}, gamer
 add_gamer({'name':'Latasha Bryan','availability': ["Monday", "Sunday"]}, gamers)
 add_gamer({'name':'Crystal Brewer','availability': ["Thursday", "Friday", "Saturday"]}, gamers)
 add_gamer({'name':'James Barnes Jr.','availability': ["Tuesday", "Wednesday", "Thursday", "Sunday"]}, gamers)
-print(add_gamer({'name':'Michel Trujillo','availability': ["Monday", "Tuesday", "Wednesday"]}, gamers))
+add_gamer({'name':'Michel Trujillo','availability': ["Monday", "Tuesday", "Wednesday"]}, gamers)
+print(gamers)
 
 count_availability = build_daily_frequency_table()
-print(count_availability)
+#print(count_availability)
+print(calculate_availability(gamers, count_availability))
