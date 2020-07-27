dde43d581615d98c0b37e3639c52328c60139c6d
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-07-27 13:11:52 +0100

    Populated gamers list using add _gamer

--- Dictionaries/Abruptly_Goblins_Planner.py
+++ Dictionaries/Abruptly_Goblins_Planner.py
@@ -6,4 +6,13 @@ def add_gamer(gamer, gamers_list):
     return gamers_list
 
 kimberly = {"name": "Kimberly Warner", "availability": ["Mondays", "Tuesdays", "Firdays"]}
-print(add_gamer(kimberly, gamers))
+#print(add_gamer(kimberly, gamers))
+add_gamer({'name':'Thomas Nelson','availability': ["Tuesday", "Thursday", "Saturday"]}, gamers)
+add_gamer({'name':'Joyce Sellers','availability': ["Monday", "Wednesday", "Friday", "Saturday"]}, gamers)
+add_gamer({'name':'Michelle Reyes','availability': ["Wednesday", "Thursday", "Sunday"]}, gamers)
+add_gamer({'name':'Stephen Adams','availability': ["Thursday", "Saturday"]}, gamers)
+add_gamer({'name': 'Joanne Lynn', 'availability': ["Monday", "Thursday"]}, gamers)
+add_gamer({'name':'Latasha Bryan','availability': ["Monday", "Sunday"]}, gamers)
+add_gamer({'name':'Crystal Brewer','availability': ["Thursday", "Friday", "Saturday"]}, gamers)
+add_gamer({'name':'James Barnes Jr.','availability': ["Tuesday", "Wednesday", "Thursday", "Sunday"]}, gamers)
+print(add_gamer({'name':'Michel Trujillo','availability': ["Monday", "Tuesday", "Wednesday"]}, gamers))
