3a4b53c126ba642851e36fddf4e55517185bb40f
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-07-27 13:08:12 +0100

    Added function add_gamer

--- Dictionaries/Abruptly_Goblins_Planner.py
+++ Dictionaries/Abruptly_Goblins_Planner.py
@@ -1,1 +1,9 @@
 gamers = []
+
+def add_gamer(gamer, gamers_list):
+    if "name" in gamer and "availability" in gamer:
+        gamers_list.append(gamer)
+    return gamers_list
+
+kimberly = {"name": "Kimberly Warner", "availability": ["Mondays", "Tuesdays", "Firdays"]}
+print(add_gamer(kimberly, gamers))
