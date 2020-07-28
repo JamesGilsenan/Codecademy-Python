006444a6cee47824d5628e32073756b985bfaaa7
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-07-28 20:20:17 +0100

    Added each username to list compromised_users

--- Files/Files_Hacking_The_Fender.py
+++ Files/Files_Hacking_The_Fender.py
@@ -4,4 +4,5 @@ compromised_users = []
 with open("passwords.csv") as password_file:
     password_csv = csv.DictReader(password_file)
     for password_row in password_csv:
-        print(password_row["Username"])
+        compromised_users.append(password_row["Username"])
+    print(compromised_users)
