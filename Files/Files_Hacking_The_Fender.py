719ae9f2b5026b0e7ef4f69c74da2c73b2f6cd97
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-07-28 20:27:00 +0100

    Open json file in write mode

--- Files/Files_Hacking_The_Fender.py
+++ Files/Files_Hacking_The_Fender.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 compromised_users = []
 with open("passwords.csv") as password_file:
@@ -10,3 +11,6 @@ with open("passwords.csv") as password_file:
 with open("compromised_users", "w") as compromised_user_file:
     for user in compromised_users:
         compromised_user_file.write(user)
+
+with open("boss_message.json", "w") as boss_message:
+    
