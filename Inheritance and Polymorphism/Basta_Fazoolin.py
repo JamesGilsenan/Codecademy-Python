59db018c9c7977caa19a67b7a6d7aff5f8442dd5
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-08-06 21:27:42 +0100

    New class Franchises and constructor

--- Inheritance and Polymorphism/Basta_Fazoolin.py
+++ Inheritance and Polymorphism/Basta_Fazoolin.py
@@ -1,5 +1,4 @@
 class Menu:
-    
     def __init__(self, name, items, start_time, end_time):
         self.name = name
         self.items = items
@@ -14,6 +13,12 @@ class Menu:
         for item in purchased_items:
             total += self.items[item]
         return "Your total bill for {} is: €{}".format(self.name, total)
+
+
+class Franchise():
+    def __init__(self, address, menus):
+        self.address = address
+        self.menus = menus
 
 brunch = Menu("Brunch", {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}
 , "11am", "4pm")
