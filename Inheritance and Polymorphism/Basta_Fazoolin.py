ba28ebf73e73bd4f7f342e705f330b5956e10a21
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-08-06 21:33:05 +0100

    Initialized 2 franchises

--- Inheritance and Polymorphism/Basta_Fazoolin.py
+++ Inheritance and Polymorphism/Basta_Fazoolin.py
@@ -20,11 +20,17 @@ class Franchise():
         self.address = address
         self.menus = menus
 
+
 brunch = Menu("Brunch", {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}
 , "11am", "4pm")
 early_bird = Menu("Early Bird", {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 
 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,}, "3pm", "6pm")
+dinner = ("Dinner", {'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 
+'coffee': 2.00, 'espresso': 3.00,}, "5pm", "11pm")
 kids = Menu("Kids", {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}, "11am", "9pm")
+
+flagship_store = Franchise("1232 West End Road", [brunch, early_bird, dinner, kids])
+new_installment = Franchise("12 East Mulberry Street", [brunch, early_bird, dinner, kids])
 
 print(brunch)
 print(early_bird)
