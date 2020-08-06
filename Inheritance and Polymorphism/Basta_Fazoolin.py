fb3750d9d55564dec1296c5a6e92fc5cd7b7f4b7
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-08-06 20:43:14 +0100

    Created 3 menus. Brunch, early bird and kids

--- Inheritance and Polymorphism/Basta_Fazoolin.py
+++ Inheritance and Polymorphism/Basta_Fazoolin.py
@@ -1,8 +1,13 @@
 class Menu:
     
-    def __init__(self, names, items, start_time, end_time):
-        self.names = names
+    def __init__(self, name, items, start_time, end_time):
+        self.name = name
         self.items = items
         self.start_time = start_time
         self.end_time = end_time
-        
+
+brunch = Menu("Brunch", {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}
+, 11.00, 16.00)
+early_bird = Menu("Early Bird", {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 
+'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,}, 15.00, 18.00)
+kids = Menu("Kids", {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}, 11.00, 21.00)
