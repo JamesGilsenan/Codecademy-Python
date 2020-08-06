cbf76da96c96868db8d9ee454bcf0d5edba1d2a4
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-08-06 14:59:02 +0100

    Demo of sorting list using append method

--- Inheritance and Polymorphism/Inheritance_and_Polymorphism.py
+++ Inheritance and Polymorphism/Inheritance_and_Polymorphism.py
@@ -1,5 +1,11 @@
 class SortedList(list):
 
-  def append(self, value):
-    super().append(value)
-    self.sort()
+    def append(self, value):
+        super().append(value)
+        self.sort()
+        return self
+
+mylist = SortedList([1,3,5])
+print(mylist)
+mylist.append(2)
+print(mylist)
