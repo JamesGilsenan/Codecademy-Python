0e16a4be41b15dcdbf187bdb19943dcdd0e1117d
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-08-06 15:32:12 +0100

    Override dict .get() Returns string if get fails

--- Inheritance and Polymorphism/Inheritance_and_Polymorphism.py
+++ Inheritance and Polymorphism/Inheritance_and_Polymorphism.py
@@ -9,7 +9,22 @@ class SortedList(list):
         self.sort()
         return self
 
+
+class FallbackDict(dict):
+    
+    def get(self, key):
+        fallback_value = "Key does not exist"
+        super().get(key, fallback_value)
+        if key in self:
+            return key
+        else:
+            return fallback_value        
+
+
 mylist = SortedList([1,5,3,9])
-print(mylist)
+#print(mylist)
 mylist.append(2)
-print(mylist)
+#print(mylist)
+
+mydict = FallbackDict({"Mary": 20, "George": 18, "Jack": 12})
+print(mydict.get("John"))
