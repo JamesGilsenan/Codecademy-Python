60085623d9b9efb90a7d055a2ff6a55e84876026
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-08-06 20:33:54 +0100

    Starting resteraunt project Basta Fazoolin'

---
+++ Inheritance and Polymorphism/Basta_Fazoolin.py
@@ -0,0 +1,8 @@
+class Menu:
+    
+    def __init__(self, names, items, start_time, end_time):
+        self.names = names
+        self.items = items
+        self.start_time = start_time
+        self.end_time = end_time
+        
