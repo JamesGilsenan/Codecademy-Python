99fbb4ce97a534f215a666fe42c5672f61a5f98a
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-08-06 15:12:03 +0100

    Override constructor. List sorted when initialized

--- Inheritance and Polymorphism/Inheritance_and_Polymorphism.py
+++ Inheritance and Polymorphism/Inheritance_and_Polymorphism.py
@@ -1,11 +1,15 @@
 class SortedList(list):
 
+    def __init__(self, list):
+        super().__init__(list)
+        self.sort()
+
     def append(self, value):
         super().append(value)
         self.sort()
         return self
 
-mylist = SortedList([1,3,5])
+mylist = SortedList([1,5,3,9])
 print(mylist)
 mylist.append(2)
 print(mylist)
