e8b5e0650bb06de06451c91f76d3f1bcb50678bb
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-08-07 22:09:23 +0100

    Keyword Argument Unpacking

---
+++ Function_Arugments/Keyword_Argument_Unpacking.py
@@ -0,0 +1,10 @@
+print("My name is {name} and I'm feeling {feeling}.".format(name="Yon Lad", feeling="Yabadabadoo"))
+
+# Checkpoint 2
+from products import create_product
+
+def create_products(**products_dict):
+  for name, price in products_dict.items():
+    create_product(name, price)
+
+create_products(Baseball=3, Shirt = 14, Guitar = 70)
