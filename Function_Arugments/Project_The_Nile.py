2fcfa7c1cf319803229b1bdee7bdc5490fa3015e
James Gilsenan <jamesgilsenan37@gmail.com>
2020-08-09 19:47:19 +0100

    Calculated distance and 2nd way to unpack

--- Function_Arugments/Project_The_Nile.py
+++ Function_Arugments/Project_The_Nile.py
@@ -4,8 +4,10 @@ from Test import test_function
 def calculate_shipping_cost(from_coords, to_coords, shipping_type):
     from_long, from_lat = from_coords
     to_long, to_lat = to_coords
-    print(from_long, from_lat)
-    print(to_long, to_lat)
-    
+    #distance = get_distance(*from_coords, *to_coords) also unpacks variables and works the same as line below
+    distance = get_distance(from_lat, from_long, to_lat, to_long)
+    print(distance)
+
+
 
 calculate_shipping_cost([1.147865, 4.578456], [1.56843, 2.84384], "Steam")
