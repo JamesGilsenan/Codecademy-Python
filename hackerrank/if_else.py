764ba7f21c5065f73c370b67184dc0ab67280071
James Gilsenan <jamesgilsenan37@gmail.com>
2021-04-12 16:53:57 +0100

    Operators +-*/

--- hackerrank/if_else.py
+++ hackerrank/if_else.py
@@ -6,7 +6,7 @@ if n % 2 == 1:
     print(weird)
 elif n % 2 == 0 and n >= 2 and n <= 5:
     print(not_weird)
-elif n % 2 == 0 and n in range(6, 20):
+elif n % 2 == 0 and n in range(6, 21):
     print(weird)
 elif n % 2 == 0 and n > 20:
     print(not_weird)
